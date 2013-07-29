ec092ffa6a51953d8e35f73d8c0ef3ca70f64574
An Bao NGUYEN <annb@exoplatform.com>
2013-07-29 13:54:11 +0700

    Remove temporaritly gatein-portal properties

--- translations/src/config/projects.sh
+++ translations/src/config/projects.sh
@@ -17,7 +17,7 @@
 # 02110-1301 USA, or see the FSF site: http://www.fsf.org.
 #
 
-projects=( 'platform' 'platform-ui' 'ecms' 'calendar' 'forum' 'wiki' 'social' 'commons' 'integration' 'gatein-portal' )
-versions=( '4.0.x' '4.0.x' '4.0.x' '4.0.x' '4.0.x' '4.0.x' '4.0.x' '4.0.x' '4.0.x' '3.5.x' )
+projects=( 'platform' 'platform-ui' 'ecms' 'calendar' 'forum' 'wiki' 'social' 'commons' 'integration' )
+versions=( '4.0.x' '4.0.x' '4.0.x' '4.0.x' '4.0.x' '4.0.x' '4.0.x' '4.0.x' '4.0.x' )
 
 echo "WORKING PROJECTS = [${projects[@]}]"
